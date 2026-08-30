62071d7da96ccc90ececdd1ce8e9987a4edb7d3d
Kaleidox <git@kaleidox.de>
2026-08-30 12:57:53 +0200

    move link creation method to registry

--- src/PipeWire.NET/PipeWireRegistry.cs
+++ src/PipeWire.NET/PipeWireRegistry.cs
@@ -109,6 +109,58 @@ public sealed class PipeWireRegistry : IAsyncDisposable
         }
     }
 
+    /// <summary>
+    /// todo: write docs
+    /// </summary>
+    /// <param name="feed"></param>
+    /// <param name="sink"></param>
+    /// <exception cref="Exception"></exception>
+    public unsafe Task<PipeWireLink> CreateLink(PipeWirePort feed, PipeWirePort sink)
+    {
+        if (feed._registry != this || sink._registry != this)
+            throw new ArgumentException($"Linking ports {feed.PortId} and {sink.PortId} not possible; registry mismatch");
+        if (feed.PortDirection != PipeWirePortDirection.Out)
+            throw new ArgumentException($"Linking ports {feed.PortId} and {sink.PortId} not possible; {nameof(feed)} is not an output port");
+        if (sink.PortDirection != PipeWirePortDirection.In)
+            throw new ArgumentException($"Linking ports {feed.PortId} and {sink.PortId} not possible; {nameof(sink)} is not an input port");
+
+        spa_interface* result;
+
+        fixed (byte* pkin = Encoding.UTF8.GetBytes(Native.PW_KEY_LINK_OUTPUT_NODE))
+        fixed (byte* pvin = Encoding.UTF8.GetBytes(feed.NodeId.ToString()))
+        fixed (byte* pkip = Encoding.UTF8.GetBytes(Native.PW_KEY_LINK_OUTPUT_PORT))
+        fixed (byte* pvip = Encoding.UTF8.GetBytes(feed.PortId.ToString()))
+        fixed (byte* pkon = Encoding.UTF8.GetBytes(Native.PW_KEY_LINK_INPUT_NODE))
+        fixed (byte* pvon = Encoding.UTF8.GetBytes(sink.NodeId.ToString()))
+        fixed (byte* pkop = Encoding.UTF8.GetBytes(Native.PW_KEY_LINK_INPUT_PORT))
+        fixed (byte* pvop = Encoding.UTF8.GetBytes(sink.PortId.ToString()))
+        {
+            var inputNode = new spa_dict_item { key = (sbyte*)pkin, value = (sbyte*)pvin };
+            var inputPort = new spa_dict_item { key = (sbyte*)pkip, value = (sbyte*)pvip };
+            var outputNode = new spa_dict_item { key = (sbyte*)pkon, value = (sbyte*)pvon };
+            var outputPort = new spa_dict_item { key = (sbyte*)pkop, value = (sbyte*)pvop };
+
+            fixed (spa_dict_item* ptr = new[] { inputNode, inputPort, outputNode, outputPort })
+            {
+                var dict = new spa_dict { flags = 0, items = ptr, n_items = 4 };
+
+                Native.GetInterface(_ctx._core, out pw_core_methods* methods, out void* data);
+                fixed (byte* key = "link-factory"u8.ToArray())
+                fixed (byte* iface = Encoding.UTF8.GetBytes(Native.PW_TYPE_INTERFACE_Link))
+                fixed (spa_dict* props = new[] { dict })
+                    using (_ctx.Lock())
+                        result = (spa_interface*)methods->create_object(data, (sbyte*)key, (sbyte*)iface, Native.PW_VERSION_LINK, props, 0);
+            }
+        }
+
+        if ((byte)result == 0)
+        {
+            throw new Exception($"Creating new link from port {feed.PortId} to port {sink.PortId} failed!");
+        }
+
+        return WaitForLink(feed.PortId, sink.PortId);
+    }
+
     /// <inheritdoc/>
     public ValueTask DisposeAsync()
     {
